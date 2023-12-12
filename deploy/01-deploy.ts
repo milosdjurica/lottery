0ec475b2a627721354b1ac418385e7e76d049bed
milosdjurica <milosdjurica13@gmail.com>
2023-12-12 03:38:16 +0100

    succesfully deployed mocks and lottery on dev chain

--- deploy/01-deploy.ts
+++ deploy/01-deploy.ts
@@ -1,7 +1,8 @@
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { networkConfig } from "../utils/helper-config";
+import { developmentChains, networkConfig } from "../utils/helper-config";
 import { VRFCoordinatorV2Mock } from "../typechain-types";
+import { EventLog } from "ethers";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 	const { ethers, getNamedAccounts, deployments, network } = hre;
@@ -21,10 +22,37 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 	let vrfCoordinatorV2Mock: VRFCoordinatorV2Mock;
 	let vrfCoordinatorV2Address: string;
 
+	if (developmentChains.includes(network.name)) {
+		vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock");
+		vrfCoordinatorV2Address = await vrfCoordinatorV2Mock.getAddress();
+		const txResponse = await vrfCoordinatorV2Mock.createSubscription();
+		const txReceipt = await txResponse.wait(1);
+		subscriptionId = await (txReceipt?.logs[0] as EventLog).args.subId;
+		await vrfCoordinatorV2Mock.fundSubscription(
+			subscriptionId,
+			VRF_SUB_FUND_AMOUNT,
+		);
+	} else {
+		vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2!;
+		subscriptionId = networkConfig[chainId].subscriptionId;
+	}
+
+	const BLOCK_CONFIRMATIONS = developmentChains.includes(network.name) ? 1 : 3;
+
+	const constructorArgs = [
+		vrfCoordinatorV2Address,
+		TICKET_PRICE,
+		gasLane,
+		subscriptionId,
+		callbackGasLimit,
+	];
+
+	log("Deploying lottery contract....");
 	const lottery = await deploy("Lottery", {
 		from: deployer,
-		args: [ticketPrice], // ! constructor args
+		args: constructorArgs,
 		log: true,
+		waitConfirmations: BLOCK_CONFIRMATIONS,
 	});
 
 	log(`Lottery contract: `, lottery.address);
