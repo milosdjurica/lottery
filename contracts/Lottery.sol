1baa4ee69f6fa0c13484b54ef66bde0ab4e7c62e
milosdjurica <milosdjurica13@gmail.com>
2023-12-12 02:04:02 +0100

    added VRFConsumer

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -6,8 +6,10 @@ pragma solidity ^0.8.20;
 ////////////////////
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
+import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
-contract Lottery {
+contract Lottery is VRFConsumerBaseV2 {
 	////////////////////
 	// * Errors 	  //
 	////////////////////
@@ -15,6 +17,7 @@ contract Lottery {
 	error Lottery__NotOpen();
 	error Lottery__TransactionFailed();
 	error Lottery__AlreadyFull();
+	error Lottery__TransferFailed();
 
 	////////////////////
 	// * Types 		  //
@@ -28,17 +31,30 @@ contract Lottery {
 	// * Variables	  //
 	////////////////////
 
-	uint private immutable i_ticketPrice;
+	// * Constants
+	uint32 private constant NUM_WORDS = 1;
+	uint16 private constant REQUEST_CONFIRMATIONS = 3;
 
-	LotteryState private s_lotteryState;
+	// * Immutable
+	uint private immutable i_ticketPrice;
+	VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+	bytes32 private immutable i_gasLane;
+	uint64 private immutable i_subscriptionId;
+	uint32 private immutable i_callbackGasLimit;
+
+	// * Storage
 	address payable[] private s_players;
+	LotteryState private s_lotteryState;
 	mapping(address player => bool startEarly) public s_startEarly;
+	address private s_recentWinner;
 
 	////////////////////
 	// * Events 	  //
 	////////////////////
 	event LotteryEnter(address indexed player, uint indexed numOfPlayers);
 	event PickingWinner(LotteryState indexed lotteryState);
+	event WinnerPicked(address indexed winner);
+	event RequestedNumber(uint indexed requestId);
 
 	////////////////////
 	// * Modifiers 	  //
@@ -57,9 +73,18 @@ contract Lottery {
 	////////////////////
 	// * Constructor  //
 	////////////////////
-	constructor(uint _ticketPrice) {
+	constructor(
+		address _vrfCoordinator,
+		uint _ticketPrice,
+		bytes32 _gasLane,
+		uint64 _subscriptionId,
+		uint32 _callbackGasLimit
+	) VRFConsumerBaseV2(_vrfCoordinator) {
 		i_ticketPrice = _ticketPrice;
-
+		i_vrfCoordinator = VRFCoordinatorV2Interface(_vrfCoordinator);
+		i_subscriptionId = _subscriptionId;
+		i_callbackGasLimit = _callbackGasLimit;
+		i_gasLane = _gasLane;
 		s_lotteryState = LotteryState.OPEN;
 	}
 
@@ -88,10 +113,13 @@ contract Lottery {
 		}
 	}
 
-	function leave() public stateIsOpen {}
+	function leave() public stateIsOpen {
+		// player can leave and get his money back if he doesnt want to wait for others anymore
+	}
 
 	function pickWinnerEarlier() internal {
 		// only called if 2 players agree to start lottery earlier (not wait 3rd player, and play 1v1)
+		// if both agree call pickWinner()
 	}
 
 	////////////////////
@@ -100,7 +128,29 @@ contract Lottery {
 	function pickWinner() internal {
 		s_lotteryState = LotteryState.CLOSED;
 		emit PickingWinner(s_lotteryState);
-		payable(msg.sender).transfer(address(this).balance);
+		uint requestId = i_vrfCoordinator.requestRandomWords(
+			i_gasLane,
+			i_subscriptionId,
+			REQUEST_CONFIRMATIONS,
+			i_callbackGasLimit,
+			NUM_WORDS
+		);
+		emit RequestedNumber(requestId);
+	}
+
+	// ! This function executes after requested number is created
+	function fulfillRandomWords(
+		uint, // requestId,
+		uint[] memory randomWords
+	) internal override {
+		uint winnerIndex = randomWords[0] % s_players.length;
+		address payable recentWinner = s_players[winnerIndex];
+		s_recentWinner = recentWinner;
+		s_lotteryState = LotteryState.OPEN;
+		delete s_players;
+		emit WinnerPicked(recentWinner);
+		(bool success, ) = recentWinner.call{value: address(this).balance}("");
+		if (!success) revert Lottery__TransferFailed();
 	}
 
 	////////////////////
