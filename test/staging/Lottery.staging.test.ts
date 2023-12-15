2386b1ac159ca9141caf1c0352013e6fbda61e3e
milosdjurica <milosdjurica13@gmail.com>
2023-12-15 21:06:30 +0100

    removed balance checks and added mapping WantsToStartEarly back to NONE checks

--- test/staging/Lottery.staging.test.ts
+++ test/staging/Lottery.staging.test.ts
@@ -9,14 +9,14 @@ const isDevelopmentChain = developmentChains.includes(network.name);
 isDevelopmentChain
 	? describe.skip
 	: describe("Lottery Staging Tests", () => {
-			console.log("Entered test");
+			console.log("Entered Staging Tests");
 			const CHAIN_ID = network.config.chainId!!!;
 			const TICKET_PRICE = networkConfig[CHAIN_ID].lotteryTicketPrice;
 
 			let lottery: Lottery;
 			let deployer: string;
-			let playerAccount: HardhatEthersSigner;
 			let accounts: HardhatEthersSigner[];
+			let playerAccount: HardhatEthersSigner;
 
 			beforeEach(async () => {
 				console.log("Before Each");
@@ -27,7 +27,7 @@ isDevelopmentChain
 			});
 
 			describe("FulfillRandomWords Staging Tests", () => {
-				it("Works with Chainlink VRF, and picks random winner", async () => {
+				it("Works with Chainlink VRF, picks random winner, and after that puts LotteryState to OPEN, removes players from array, and resets mapping WantsToPickEarly to NONE", async () => {
 					const event = lottery.getEvent("WinnerPicked");
 
 					await new Promise<void>(async (resolve, reject) => {
@@ -37,58 +37,32 @@ isDevelopmentChain
 								const recentWinner = await lottery.getRecentWinner();
 								console.log("recentWinner", recentWinner);
 								const lotteryState = await lottery.getLotteryState();
-								console.log("lotteryState", lotteryState);
-								let deployerBalanceAfter;
-								let player1BalanceAfter;
-								await new Promise<void>(async (resolve) => {
-									deployerBalanceAfter = await ethers.provider.getBalance(
-										accounts[0],
-									);
-									console.log("deployerBalanceAfter", deployerBalanceAfter);
-									player1BalanceAfter = await ethers.provider.getBalance(
-										accounts[1],
-									);
-									console.log("player1BalanceAfter", player1BalanceAfter);
-									resolve();
-								});
-								assert.equal(await lottery.getNumOfActivePlayers(), BigInt(0));
-								assert.equal(lotteryState, BigInt(0));
-								// ! Commented tests are failing, probably because of gas costs of transactions
-								if (recentWinner === accounts[0].address) {
-									assert.equal(recentWinner, accounts[0].address);
-									// assert.equal(
-									// 	deployerBalanceAfter,
-									// 	deployerBalanceStart + TICKET_PRICE,
-									// );
-									// assert.equal(player1BalanceAfter, player1BalanceStart);
-								} else {
-									assert.equal(recentWinner, accounts[1].address);
-									// assert.equal(
-									// 	player1BalanceAfter,
-									// 	player1BalanceStart + TICKET_PRICE,
-									// );
-									// assert.equal(deployerBalanceAfter, deployerBalanceStart);
-								}
 
+								assert.equal(lotteryState, BigInt(0));
+								assert.equal(await lottery.getNumOfActivePlayers(), BigInt(0));
+								assert.equal(
+									await lottery.getPlayerWantsToStart(accounts[0]),
+									BigInt(0),
+								);
+
+								assert.equal(
+									await lottery.getPlayerWantsToStart(accounts[1]),
+									BigInt(0),
+								);
 								resolve();
 							} catch (error) {
 								reject(error);
 							}
 						});
-						console.log("Welcome to test");
-						await lottery.enterLottery({ value: TICKET_PRICE });
+
+						console.log("Starting first test");
+						await lottery.enterLottery({
+							value: TICKET_PRICE,
+						});
 						await lottery
 							.connect(playerAccount)
 							.enterLottery({ value: TICKET_PRICE });
 						console.log("Players entered lottery");
-						const deployerBalanceStart = await ethers.provider.getBalance(
-							accounts[0],
-						);
-						const player1BalanceStart = await ethers.provider.getBalance(
-							accounts[1],
-						);
-						console.log("Deployer balance start -> ", deployerBalanceStart);
-						console.log("Player balance start -> ", player1BalanceStart);
 					});
 				});
 			});
